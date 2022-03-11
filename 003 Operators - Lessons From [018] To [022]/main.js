19dba2b9c881d3b29471d63aa2ed54f99f80f163
MoHosam <hosamation15@gmail.com>
2022-03-11 15:21:07 +0200

    Assignment[3] is Done

---
+++ 003 Operators - Lessons From [018] To [022]/main.js
@@ -0,0 +1,44 @@
+// Assignment 01 
+console.log('%cAssignment 01', 'font-size:20px; font-weight:bold');
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 + 15 % 3 + 190 + 10 - 400); // 0
+
+// Assignment 02
+console.log('%cAssignment 02', 'font-size:20px; font-weight:bold');
+
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * num - num); // 6
+
+// Solution Three
+console.log((num * num + num * num) / num); // 6
+
+// Solution Four
+console.log(num * true + num); // 6
+
+// Solution Five
+console.log(num + false + num); // 6
+
+// Solution Six
+console.log(num ** num / num - num); // 6
+
+// Assignment 03
+console.log('%cAssignment 03', 'font-size:20px; font-weight:bold');
+
+let num_ = "10";
+
+// Solution One
+console.log(+num_ + +num_); // 20
+
+// Solution Two
+console.log(+num_ - +num_ - -num_ - -num_ ); // 20
+
+// Solution Three
+console.log(+num_ * true + +num_); // 20
+
+// Solution Four
+console.log(+num_ * (true + true)); // 20
