5c223a585b025e337bcb2087b5b0db7a8738bb03
MoHosam <hosamation15@gmail.com>
2022-03-12 14:51:55 +0200

    Play With Variables Values is Done

---
+++ Challenges/002 Play With Variables Values Challenge/main.js
@@ -0,0 +1,12 @@
+console.log('%cPlay With Variables Values Challenge', 'font-size:20px; font-weight:bold');
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+// Only Use Variables Value
+// Do Not Use Variable Twice
+
+console.log(-d * +e); // 2000
+console.log(-d + ++f + ++e * ++g); // 173
