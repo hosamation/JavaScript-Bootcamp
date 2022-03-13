b7ac302af259d7c98bf6a32ec2a60b082702b8ec
MoHosam <hosamation15@gmail.com>
2022-03-13 23:20:19 +0200

    Done

---
+++ Challenges/003 Numbers From Variables Challenge/main.js
@@ -0,0 +1,22 @@
+console.log('%cNumbers From Variables Challenge', 'font-size:20px; font-weight:bold');
+
+let a = 100;
+let b = 2_00.5;
+let c = 1e2;
+let d = 2.4;
+
+// Find Smallest Number In All Variables And Return Integer
+console.log(Math.trunc(Math.min(a, b, c, d)));
+
+// Use Variables a + d One Time To Get The Needed Output
+console.log(Math.pow(a, Math.trunc(d))); // 10000
+
+// Get Integer "2" From d Variable With 4 Methods
+console.log(parseInt(d));
+console.log(Math.trunc(d));
+console.log(Math.floor(d));
+console.log(Math.round(d));
+
+// Use Variables b + d To Get This Valus
+console.log(+(Math.trunc(b) / Math.ceil(d)).toFixed(2)); // 66.67 => String
+console.log(Math.ceil(Math.trunc(b) / Math.ceil(d))); // 67 => Number
